bb5ec1694090571dbc9a3c0a8b6e1860b1237944
RahulBorade94 <boraderahul94@gmail.com>
2022-01-12 22:37:48 +0530

    ADDED: Create Address Book System

---
+++ src/com/bridgelabz/addressbook/Contact.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.addressbook;
+
+public class Contact {
+    //Variable Declaration for Contacts Class
+    String firstName;
+    String lastname;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNo;
+    String emailID;
+}
