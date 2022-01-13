9611e18f51384b7384fe75c5d4c0e5097e5d63c1
RahulBorade94 <boraderahul94@gmail.com>
2022-01-13 15:49:26 +0530

    MODIFILED:  Edit the conatact details in AddressBook

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -3,10 +3,16 @@ package com.bridgelabz.addressbook;
 import java.util.Scanner;
 
 public class AddressBook {
-    //scanner object input purposr
+    //scanner object input purpose
+
     Scanner sc =new Scanner(System.in);
+
+    //contact class create abject
+
     Contact contact =new Contact();
 
+
+    //adding contact details in AddressBook using addMethod
     public void addMethod()
     {
         System.out.println("Address Book Details:");
@@ -27,6 +33,8 @@ public class AddressBook {
         System.out.println("Enter the EMail ID :");
         contact.setEmailID(sc.next());
     }
+
+    //show the contact details in addressbook using showContact method
     public void showContact(){
         System.out.println("Contact Details -");
         System.out.println("First Name : "+contact.getFirstName());
@@ -38,5 +46,31 @@ public class AddressBook {
         System.out.println("Phone Number : "+contact.getPhoneNo());
         System.out.println("EMail ID : "+contact.getEmailID());
     }
+
+    //edit conatct in addressbook editContcat method
+    public void editContact(){
+        System.out.println("Enter a Name");
+        String FirstName=sc.next();
+        if(!FirstName.equalsIgnoreCase(contact.getFirstName())) {
+            System.out.println("The Entered Contact Name is Not Available in Address Book");
+        } else {
+            System.out.println("Enter the Last Name :");
+            contact.setLastname(sc.next());
+            System.out.println("Enter the Address :");
+            contact.setAddress(sc.next());
+            System.out.println("Enter the City :");
+            contact.setCity(sc.next());
+            System.out.println("Enter the State :");
+            contact.setState(sc.next());
+            System.out.println("Enter the Zip Code :");
+            contact.setZip(sc.next());
+            System.out.println("Enter the Phone Number :");
+            contact.setPhoneNo(sc.next());
+            System.out.println("Enter the EMail ID :");
+            contact.setEmailID(sc.next());
+        }
+    }
+
 }
+
 
--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -3,13 +3,15 @@ package com.bridgelabz.addressbook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book System");
-        //varibale decleartion
+        //variable declaration
         int choice;
-      //Addressbook class method object created
+
+      //AddressBook class method object created
+
         AddressBook addressBook =new AddressBook();
         do {
             System.out.println("Address Book System");
-            System.out.println("1. Add New Contact\n2. Show Contact\n3. EXIT");
+            System.out.println("1. Add New Contact\n2. Show Contact\n3. Edit Contact \n4. Exit");
             System.out.println("Select Any Number: ");
             choice = addressBook.sc.nextInt();
 
@@ -21,13 +23,16 @@ public class AddressBookMain {
                     addressBook.showContact();
                     break;
                 case 3:
+                    addressBook.editContact();
+                    break;
+                case 4:
                     System.out.println("Address Book.");
                     break;
                 default:
-                    System.out.println(" Select the Number between 1 to 3");
+                    System.out.println(" Select the Number between 1 to 4");
                     break;
             }
-        }while( choice != 3 );
+        }while( choice != 4 );
 
     }
     }
